e76dee5a856cf9a172838c9424853df5a1aea1d5
christoffermagnusson <christoffer88@live.se>
2016-06-29 18:54:32 +0200

    Added workaround to get StringProperty to work with fx tableview

--- src/domain/models/Ingredient.java
+++ src/domain/models/Ingredient.java
@@ -4,16 +4,20 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 public class Ingredient {
 
 	private final ObjectProperty<IngredientType> type;
 	private final IntegerProperty amount;
+	private final StringProperty ingredientName;
 
 
 	public Ingredient(IngredientType type, int amount){
 		this.type = new SimpleObjectProperty<IngredientType>(type);
 		this.amount = new SimpleIntegerProperty(amount);
+		this.ingredientName = new SimpleStringProperty(type.getName());
 	}
 
 	public IngredientType getType(){
@@ -33,6 +37,9 @@ public class Ingredient {
 	}
 	public IntegerProperty amountProperty(){
 		return amount;
+	}
+	public StringProperty ingredientNameProperty(){
+		return ingredientName;
 	}
 
 }
--- src/domain/models/IngredientType.java
+++ src/domain/models/IngredientType.java
@@ -35,6 +35,9 @@ public class IngredientType {
 	public StringProperty measurementProperty(){
 		return measurement;
 	}
+	public String toString(){
+		return this.name.get();
+	}
 
 
 
