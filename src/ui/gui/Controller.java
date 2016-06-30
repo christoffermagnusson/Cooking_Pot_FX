73e9d1285dd00dbc7fb636b41fb7fc16fbb89f5a
christoffermagnusson <christoffer88@live.se>
2016-06-30 15:07:27 +0200

    Created a interface for Controllers, with one method; setStorages. Makes it easier to handle in MainApp-class.

---
+++ src/ui/gui/Controller.java
@@ -0,0 +1,9 @@
+package ui.gui;
+
+import storage.interfaces.*;
+
+public interface Controller {
+	
+	void setStorages(ChefStorage chefStorage,IngredientStorage ingredientStorage,RecipeStorage recipeStorage);
+
+}
