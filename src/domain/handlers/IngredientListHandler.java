93acd730d1013a27d8193c7f152694f61d6796e0
christoffermagnusson <christoffer88@live.se>
2016-07-09 00:34:23 +0200

    Added functionality for handling recipe ingredients

--- src/domain/handlers/IngredientListHandler.java
+++ src/domain/handlers/IngredientListHandler.java
@@ -8,23 +8,29 @@ import log.Log;
 
 public class IngredientListHandler {
 
-	private Recipe recipe;
+
 	ArrayList<Ingredient> ingredientList = new ArrayList<Ingredient>();
 	Log log = new Log();
 
-	public IngredientListHandler(Recipe recipe){
-		this.recipe=recipe;
+	public IngredientListHandler(){
+
 	}
 
 	public void addIngredient(Ingredient ingredient){
-		ingredientList.add(ingredient);
-		// here should be some storage statement.. perhaps class is redundant
-		log.write(String.format("%s added to %s",ingredient.toString(),this.recipe.toString()));
-	}
+			log.write(String.format("%s added to recipe.",ingredient.toString()));
+			ingredientList.add(ingredient);
+			}
+
 	public ArrayList<Ingredient> getIngredientList(){
 		return ingredientList;
 	}
-	public Recipe getRecipe(){
-		return recipe;
+	public boolean checkList(Ingredient ingredient){
+		for(Ingredient i : ingredientList){
+			if(i.toString().equals(ingredient.toString())){
+				return false;
+			}
+		}
+		return true;
 	}
+
 }
