9f0e0afb91e0d7a76215b40fede6589c135dbd85
christoffermagnusson <christoffer88@live.se>
2016-08-02 00:32:18 +0200

    Changed to new impl-classes instead of previous test-classes

--- src/storage/factories/IngredientStorageFactory.java
+++ src/storage/factories/IngredientStorageFactory.java
@@ -1,12 +1,13 @@
 package storage.factories;
 
 import storage.interfaces.IngredientStorage;
+import storage.impl.IngredientStorageImpl;
 import storage.impl.IngredientStorageImplTest;
 
 public class IngredientStorageFactory {
 
 	public static IngredientStorage getStorage(){
-		return new IngredientStorageImplTest();
+		return new IngredientStorageImpl();
 	}
 
 }
--- src/storage/factories/RecipeStorageFactory.java
+++ src/storage/factories/RecipeStorageFactory.java
@@ -1,11 +1,12 @@
 package storage.factories;
 
 import storage.impl.RecipeStorageImplTest;
+import storage.impl.RecipeStorageImpl;
 import storage.interfaces.RecipeStorage;
 
 public class RecipeStorageFactory {
 
 	public static RecipeStorage getStorage(){
-		return new RecipeStorageImplTest();
+		return new RecipeStorageImpl();
 	}
 }
