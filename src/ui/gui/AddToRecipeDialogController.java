89395831e7bba4c38c51fe15653c366c59e406b4
christoffermagnusson <christoffer88@live.se>
2016-08-05 12:38:40 +0200

    Working properly at the moment. Using a recipes ingredientlisthandler to get the latest added ingredient.. Might need to do this differently when editing functionality will be implemented for recipes.

---
+++ src/ui/gui/AddToRecipeDialogController.java
@@ -0,0 +1,119 @@
+package ui.gui;
+
+import storage.interfaces.ChefStorage;
+import storage.interfaces.IngredientStorage;
+import storage.interfaces.RecipeStorage;
+import ui.gui.verifier.Verifier;
+
+import java.util.ArrayList;
+
+import appl.controllers.MainApp;
+import domain.handlers.IngredientListHandler;
+import domain.models.Ingredient;
+import domain.models.IngredientType;
+import domain.models.Recipe;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory.IntegerSpinnerValueFactory;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.fxml.FXML;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+
+public class AddToRecipeDialogController extends Thread implements Controller{
+
+	@FXML
+	private Label amountLabel;
+	@FXML
+	private Label typeLabel;
+	@FXML
+	private Slider amountSlider;
+	@FXML
+	private Spinner amountSpinner;
+	@FXML
+	private Button addButton;
+	@FXML
+	private Button backButton;
+
+	private ChefStorage chefStorage;
+	private IngredientStorage ingredientStorage;
+	private RecipeStorage recipeStorage;
+	private MainApp mainApp;
+	private Verifier ver = new Verifier(); // not needed here maybe? clean this later if redundant.
+
+	private IngredientListHandler handler;
+
+	@Override
+	public void setStorages(ChefStorage chefStorage, IngredientStorage ingredientStorage, RecipeStorage recipeStorage) {
+		this.chefStorage=chefStorage;
+		this.ingredientStorage=ingredientStorage;
+		this.recipeStorage=recipeStorage;
+
+	}
+
+	public void setMainApp(MainApp mainApp){
+		this.mainApp=mainApp;
+	}
+	public void setHandler(IngredientListHandler handler){
+		this.handler=handler;
+	}
+	public void initComponents(){
+		Ingredient toBeAdded = handler.getLatestAdded();
+		amountSpinner.getValueFactory().setValue(toBeAdded.getAmount());
+		amountSlider.valueProperty().setValue(toBeAdded.getAmount()); // maybe needs cast to Double
+		amountLabel.setText(String.format("Specify amount of %s",toBeAdded.getType().getName()));
+		typeLabel.setText(toBeAdded.getType().measurement());
+
+		Stage stage = (Stage) addButton.getScene().getWindow();
+		stage.setOnCloseRequest(new EventHandler<WindowEvent>(){
+			public void handle(WindowEvent we){
+				handler.deleteLatest();
+			}
+		});
+	}
+
+	@FXML
+	private void initialize(){
+		amountSpinner.setValueFactory(new IntegerSpinnerValueFactory(1,1000)); // at the moment default values 1 -> 1000 ; perhaps need an alghorithm here??
+		amountSlider.setMin(1);	amountSlider.setMax(1000);
+
+		amountSlider.valueProperty().addListener(new ChangeListener<Number>(){
+			public void changed(ObservableValue<? extends Number> ov,
+					Number old_val,Number new_val){
+				handleAmountSlider(new_val.intValue());
+			}
+		});
+
+	}
+
+	/**
+	 * Method uses handler associated with recipe that is being created to get the latest added ingredient to the list
+	 * so that the user can set the amount of ingredients according to the specified type.
+	 */
+	@FXML
+	private void handleAddButton(){
+		Ingredient toBeAdded = handler.getLatestAdded(); // get the latest added ingredient
+		toBeAdded.setAmount((Integer) amountSpinner.getValue()); // sets the value
+
+		Stage stage = (Stage) addButton.getScene().getWindow();
+		stage.close();
+	}
+	@FXML
+	private void handleBackButton(){
+		handler.deleteLatest(); // deletes this so that it will not be added to the recipe
+
+		Stage stage = (Stage) backButton.getScene().getWindow();
+		stage.close();
+	}
+
+	private void handleAmountSlider(int value){
+		amountSpinner.getValueFactory().setValue(value);
+	}
+
+
+
+}
