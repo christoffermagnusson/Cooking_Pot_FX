d23ef4074e894957900b7da27e788daf9cef70f8
christoffermagnusson <christoffer88@live.se>
2016-08-18 18:43:34 +0200

    added delete and editing gateways

--- src/ui/gui/MenuController.java
+++ src/ui/gui/MenuController.java
@@ -5,12 +5,15 @@ import storage.interfaces.IngredientStorage;
 import storage.interfaces.RecipeStorage;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Menu;
 
 import java.util.Observable;
 import java.util.Observer;
 
 import appl.controllers.MainApp;
+import domain.models.Recipe;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -34,6 +37,14 @@ public class MenuController implements Controller,Observer {
 	private MenuItem closeItem;
 	@FXML
 	private MenuItem newRecipeItem;
+
+	@FXML
+	private Menu secondMenu;
+	@FXML
+	private MenuItem editRecipeItem;
+	@FXML
+	private MenuItem deleteRecipeItem;
+
 
 	private MainApp mainApp;
 	Log log = new Log();
@@ -61,6 +72,37 @@ public class MenuController implements Controller,Observer {
 			public void handle(ActionEvent e){
 				log.write("Closing program...");
 				Platform.exit();
+			}
+		});
+		editRecipeItem.setOnAction(new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent e){
+				Recipe editRecipe = mainApp.getCurrentRecipe();
+				try{
+					if(editRecipe!=null){
+						mainApp.showEditRecipeForm(mainApp.getCurrentRecipe());
+					}
+			}catch(NullPointerException npe){
+				Alert alert = new Alert(AlertType.INFORMATION);
+				alert.setTitle("No recipe selected..");
+				alert.setHeaderText("You have not selected a recipe for editing.");
+				alert.setContentText("Please mark your selection in the list of recepies on the left side");
+				alert.showAndWait();
+			}
+		}});
+		deleteRecipeItem.setOnAction(new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent e){
+				Recipe deleteRecipe = mainApp.getCurrentRecipe();
+				try{
+					if(deleteRecipe!=null){
+						recipeStorage.deleteRecipe(deleteRecipe);
+					}
+				}catch(NullPointerException npe){
+					Alert alert = new Alert(AlertType.INFORMATION);
+					alert.setTitle("No recipe selected..");
+					alert.setHeaderText("You have not selected a recipe to delete.");
+					alert.setContentText("Please mark your selection in the list of recepies on the left side");
+					alert.showAndWait();
+				}
 			}
 		});
 
